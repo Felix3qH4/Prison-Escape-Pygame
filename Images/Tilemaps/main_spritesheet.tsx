871fc224a643ab95eafd54b134e6f90e7ecdf307
Felix3qH4 <89027442+Felix3qH4@users.noreply.github.com>
2022-03-15 21:48:47 +0100

    Add files via upload

---
+++ Images/Tilemaps/main_spritesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="main_spritesheet" tilewidth="32" tileheight="32" tilecount="19" columns="19">
+ <image source="../Spritesheets/GameImages/main_spritesheet.png" width="608" height="32"/>
+</tileset>
